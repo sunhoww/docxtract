935088d0b5ad608ddbb4e0faf745675e4f082f79
Sun Howwrongbum <sun@libermatic.com>
2018-04-01 03:19:06 +0530

    Set rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -7,5 +7,6 @@ export default [
     input: pkg.entry,
     output: [{ file: pkg.main, name: pkg.name, format: 'cjs' }],
     plugins: [babel({ exclude: 'node_modules/**' })],
+    external: ['fs', 'path', 'os', ...Object.keys(pkg.dependencies)],
   },
 ];
